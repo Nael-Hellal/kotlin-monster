8257d1eaa40d2c0e2d1bb68c0c82f1322ac93e5c
Nael-Hellal <nael.hellal.pro@gmail.com>
2025-09-18 10:00:05 +0200

    kotlin-monster

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,34 @@ fun main() {
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
     }
+    println(changeCouleur("Hello","rouge"))
+    println(changeCouleur("World","bleu"))
+    println("Hello ${changeCouleur("my","jaune")} World")
+    println(changeCouleur("Truc","marron"))
+
+}
+/**
+ * Change la couleur du message donné selon le nom de la couleur spécifié.
+ * Cette fonction utilise les codes d'échappement ANSI pour appliquer une couleur à la sortie console. Si un nom de couleur
+ * non reconnu ou une chaîne vide est fourni, aucune couleur n'est appliquée.
+ *
+ * @param message Le message auquel la couleur sera appliquée.
+ * @param couleur Le nom de la couleur à appliquer (ex: "rouge", "vert", "bleu"). Par défaut c'est une chaîne vide, ce qui n'applique aucune couleur.
+ * @return Le message coloré sous forme de chaîne, ou le même message si aucune couleur n'est appliquée.
+ */
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        "marron" -> "\u001B[m"
+        else -> "" // pas de couleur si non reconnu
+    }
+    return "$codeCouleur$message$reset"
 }
