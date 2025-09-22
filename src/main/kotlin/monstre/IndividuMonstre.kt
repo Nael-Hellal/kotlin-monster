3de602317b5dd8eaf44e00a7c0e67f5c54e9d28d
Nael-Hellal <nael.hellal.pro@gmail.com>
2025-09-22 11:27:57 +0200

    kotlin-monster

--- src/main/kotlin/monstre/IndividuMonstre.kt
+++ src/main/kotlin/monstre/IndividuMonstre.kt
@@ -14,16 +14,34 @@ class IndividuMonstre (
     var niveau: Int = 1
     var attaque: Int = espece.baseAttaque + Random.nextInt(-2, 3)
     var défense: Int = espece.baseDefense + Random.nextInt(-2, 3)
-    var vitesse: Int = espece.baseVitesse + Random.nextInt(-2,3)
-    var attaqueSpe: Int = espece.baseAttaqueSpe + Random.nextInt(-2,3)
-    var defenceSpe: Int = espece.baseDefense + Random.nextInt(-2,3)
-    var pvMax: Int = espece.basePv + Random.nextInt(-5,6)
-    var potentiel: Double = Random.nextDouble(0.5,3.0)
+    var vitesse: Int = espece.baseVitesse + Random.nextInt(-2, 3)
+    var attaqueSpe: Int = espece.baseAttaqueSpe + Random.nextInt(-2, 3)
+    var defenceSpe: Int = espece.baseDefense + Random.nextInt(-2, 3)
+    var pvMax: Int = espece.basePv + Random.nextInt(-5, 6)
+    var potentiel: Double = Random.nextDouble(0.5, 3.0)
     var exp: Double = 0.0
+        get() = field
+        set(exp) {
+            field = exp
+            var estNiveau1 = false
+            if (niveau == 1){
+                estNiveau1 = true
+            }
+            field >= palierExp(niveau)
+
+            return levelUp()
+
+            estNiveau1 == false
+
+            println("Le monstre $nom est maintenant niveau $niveau !")
+
+            if (estNiveau1 != false){
+            }
+        }
     var pv: Int = pvMax
-        get()= field
+        get() = field
         set(nouveauPv) {
-            field=nouveauPv
+            field = nouveauPv
         }
 
     /**
@@ -32,12 +50,19 @@ class IndividuMonstre (
      * @param niveau Niveau cible.
      * @return Expérience cumulée nécessaire pour atteindre ce niveau.
      */
-    fun palierExp(niveau: Int): Int{
+    fun palierExp(niveau: Int): Int {
 
-        return 100*(niveau - 1.0).pow(2).toInt()
+        return 100 * (niveau - 1.0).pow(2).toInt()
     }
-}
 
-    fun levelUp(){
-       val caracteristiques:
+
+    fun levelUp() {
+        this.niveau + 1
+        this.attaque = (this.espece.modAttaque * this.potentiel).toInt() + Random.nextInt(-2,3)
+        this.défense = (this.espece.modDefense * this.potentiel).toInt() + Random.nextInt(-2, 3)
+        this.vitesse = (this.espece.modVitesse * this.potentiel).toInt() + Random.nextInt(-2, 3)
+        this.attaqueSpe = (this.espece.modAttaqueSpe * this.potentiel).toInt() + Random.nextInt(-2, 3)
+        this.defenceSpe = (this.espece.modDefenseSpe * this.potentiel).toInt() + Random.nextInt(-2 , 3)
+        this.pvMax = (this.espece.modPv * this.potentiel).toInt() * Random.nextInt(-5, 6)
+    }
 }
