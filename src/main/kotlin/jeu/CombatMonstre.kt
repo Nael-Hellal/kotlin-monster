cc0b3a4b737c2a9f8f3b279ff8e1cff11f6b2e1c
Nael-Hellal <nael.hellal.pro@gmail.com>
2025-09-30 11:03:32 +0200

    kotlin-monster

--- src/main/kotlin/jeu/CombatMonstre.kt
+++ src/main/kotlin/jeu/CombatMonstre.kt
@@ -1,5 +1,6 @@
 package org.example.jeu
 
+import org.example.item.Utilisable
 import org.example.joueur
 import org.example.monstre.IndividuMonstre
 
@@ -29,14 +30,72 @@ class CombatMonstre(
     }
 
     fun joueurGagne(): Boolean {
-        if(monstreSauvage.pv <= 0){
-            if(monstreSauvage.entraîneur == joueur){
+        if (monstreSauvage.pv <= 0) {
+            if (monstreSauvage.entraîneur == joueur) {
                 println("${monstreSauvage.nom} a été capturé !")
                 return true
             }
             return false
         }
         println("${joueur.nom} à gagné !")
+        var gainExp = monstreSauvage.exp * 0.20
+        monstreJoueur.exp + gainExp
+        println("${monstreJoueur.nom} gagne ${gainExp} exp")
+        return true
+    }
 
+    fun actionAdversaire() {
+        if (monstreSauvage.pv > 0) monstreSauvage.attaquer(monstreJoueur)
+    }
+
+    fun actionJoueur(): Boolean {
+        if (gameOver() != true) {
+            println(
+                "menu d'action:\n" +
+                        "1: attaquer monstre sauvage \n " +
+                        "2: utiliser un item\n" +
+                        "3: changer le monstre en attaque contre un autre de l'équipe"
+            )
+            var choix = readln().toInt()
+            while (choix in (1..3)) {
+                if (choix == 1) {
+                    monstreJoueur.attaquer(monstreSauvage)
+                }
+                if (choix == 2) {
+                    println("${joueur.sacAItems}")
+                    var indexChoix = readln().toInt()
+                    var objetChoisi = joueur.sacAItems[indexChoix]
+                    if (objetChoisi is Utilisable) {
+                        var captureRéussie = objetChoisi.utiliser(monstreSauvage)
+                        if (captureRéussie == true) {
+                            return false
+                        }
+                        if (captureRéussie == false) {
+                        }
+                    }
+                    if (objetChoisi is Utilisable == false) {
+                        println("Objet non utilisable")
+                    }
+                }
+                if (choix == 3) {
+                    println("${joueur.equipeMonstre} (${monstreJoueur.pv > 0}")
+                    var indexChoix = readln().toInt()
+                    var choixMonstre = joueur.equipeMonstre[indexChoix]
+                    if (choixMonstre.pv <= 0) {
+                        println("Impossible ! Ce monstre est KO")
+                    }
+                    if (choixMonstre.pv > 0) {
+                        println("${choixMonstre} remplace ${monstreJoueur}")
+                        monstreJoueur = choixMonstre
+                    }
+                }
+            }
+            return true
+        }
+        if (gameOver()) {
+            return false
+        }
+        return true
     }
 }
+
